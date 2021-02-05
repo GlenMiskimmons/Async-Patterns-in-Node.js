9272ed5825735761242a1f77a41728246805d499
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-02-05 20:55:34 +0000

    Module 03 Writing Asynchronous Code with Promises and async/await a2fc0b8
    Utilizing async/await

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,35 +1,49 @@
 const express = require('express');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
 /* GET all clothing */
 router.route('/')
-  .get(function(req, res) {
-    getClothingData()
-      .then(data => {
-        console.log('Returning clothing data');
-        res.send(data);
-      })
-      .catch(error => res.status(500).send(error))
-      .finally(() => console.log('All done processing promise.'));
-    
-    console.log('Doing more work');
+  .get(async function(req, res) {
+
+    try {
+      let data = await getClothingData();
+      console.log('Returning async data.');
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+
+    // getClothingData()
+    //   .then(data => {
+    //     console.log('Returning clothing data');
+    //     res.send(data);
+    //   })
+    //   .catch(error => res.status(500).send(error))
+    //   .finally(() => console.log('All done processing promise.'));
   });
 
-function getClothingData() {
+async function getClothingData() {
 
-  return new Promise((resolve, reject) => {
-    fs.readFile(datafile, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        const clothingData = JSON.parse(data);
+  let rawData = await fsPromises.readFile(datafile, 'utf-8');
+  let clothingData = JSON.parse(rawData);
+
+  console.log(clothingData);
+
+  return clothingData;
+  // return new Promise((resolve, reject) => {
+  //   fs.readFile(datafile, 'utf-8', (err, data) => {
+  //     if (err) {
+  //       reject(err)
+  //     } else {
+  //       const clothingData = JSON.parse(data);
   
-       resolve(clothingData);
-      }
-    });
-  });
+  //      resolve(clothingData);
+  //     }
+  //   });
+  // });
 }
 
 module.exports = router;
